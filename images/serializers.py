a6be84d0c7975ef1c16f980c4f2da1ff76c3cb33
Adoneg <victoradoneg2@gmail.com>
2023-12-28 17:05:40 +0100

    updates

--- images/serializers.py
+++ images/serializers.py
@@ -6,4 +6,4 @@ from images.models import Product
 class ImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ('id', 'title', 'image')
+        fields = ('id', 'title', 'image', 'status', 'description', 'nutritive_benefits', 'price', 'category', 'created', )
