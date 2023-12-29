236594ebd13e610838443db3dfdf36b5b8f3ce98
Adoneg <victoradoneg2@gmail.com>
2023-12-29 23:31:05 +0100

    updates

--- api/views.py
+++ api/views.py
@@ -1,10 +1,52 @@
+# from wsgiref.util import FileWrapper
+# from api.custom_renderers import JPEGRenderer, PNGRenderer
 from rest_framework import generics
 from images.serializers import ProductSerializer, CategorySerializer
 from rest_framework.response import Response
 from images.models import Product, Category
+# from rest_framework import viewsets
+# from rest_framework import renderers
+# from rest_framework.decorators import api_view
+# from rest_framework.views import APIView
+# from rest_framework.decorators import renderer_classes, api_view
+# from rest_framework.renderers import StaticHTMLRenderer
+# from django.http import HttpResponse
+# from PIL import Image
+# # from rest_framework_api_key.permissions import HasAPIKey
 from .permissions import HasAPIKey
+# from rest_framework.views import APIView
+# from rest_framework_api_key.models import APIKey
+# # @api_view(['GET']) used for function based views
+
+
+    
+# class getImageData(generics.RetrieveAPIView, APIView):
+#     permission_classes = [HasAPIKey]
+#     queryset = Images.objects.filter(id=1)
+
+#     def get(self, request, *args, **kwargs): #request: info about the req, args n kwargs: additional info params in the req
+#         queryset = Images.objects.get(id=self.kwargs['id'])
+#         serializer = ImagesSerializer(queryset)
+#         queryset = Category.objects.get(id=serializer.data['category'])
+#         catSerializer = CategorySerializer(queryset)
+
+#         data = {
+#                 'id': self.kwargs['id'],
+#                 "cat": catSerializer.data['name'],
+#                 "title": serializer.data['title'],
+#                 "price": serializer.data['price'],
+#                 }
+#         return Response(data)
+    
+# views.py
 
 from images.serializers import ProductSerializer
+
+# class ProdListAPIView(generics.ListAPIView):
+#     permission_classes = [HasAPIKey]
+
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
 
 class ProdListAPIView(generics.ListAPIView):
     permission_classes = [HasAPIKey]
@@ -18,24 +60,22 @@ class ProdListAPIView(generics.ListAPIView):
            queryset = Category.objects.get(id=item['category'])
            catSerializer = CategorySerializer(queryset)
 
-
-            # getting a list of nutritive benefits
            nutritive_benefits = []
            str = ''
            for letter in item['nutritive_benefits']:
                if letter == ',':
-                nutritive_benefits.append(str)
-                str = ''
+                   nutritive_benefits.append(str)
+                   str=''
                else:
-                  str += letter
-
+                   str += letter
+                   
 
            data.append({
                         'id': item['id'],
                         "cat": catSerializer.data['name'],
                         "title": item['title'],
                         "price": item['price'],
-                        "product": item['image'],
+                        "image": f"https://agricma-backend.onrender.com{item['image']}",
                         "status": item['status'],
                         "description": item['description'],
                         "nutritive_benefits": nutritive_benefits,
@@ -63,7 +103,7 @@ class ProdCatListAPIView(generics.ListAPIView):
                         "cat": request.META['HTTP_CAT'].lower(),
                         "title": item['title'],
                         "price": item['price'],
-                        "product": item['image'],
+                        "image": item['image'],
                         "status": item['status'],
                         "description": item['description'],
                         "nutritive_benefits": item['nutritive_benefits'],
@@ -80,7 +120,19 @@ class CatListAPIView(generics.ListAPIView):
         catSerializer = CategorySerializer(queryset, many=True)
 
         return Response(catSerializer.data)
+    
+        print(f"\n\n{catSerializer.data}\n\n")
 
+        data = []
+        # for item in catSerializer.data:
+        #    queryset = Category.objects.get(id=item['category'])
+        #    catSerializer = CategorySerializer(queryset)
+        #    print(f"\n\n{item}\n\n")
+        #    data.append({
+        #                 'id': item['id'],
+        #                 "cat": item['name'],
+        #                 })
+        # print(data)
 
         
 
